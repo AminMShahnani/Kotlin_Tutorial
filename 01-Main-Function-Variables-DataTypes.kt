05ec511b43eaf57fe5a0b60190125a6cd24f4a84
aminmshahnani <mshahnani.amin@gmail.com>
2024-02-28 19:37:22 +0330

    Main Function, Variables, DataTypes

--- 01-Main-Function-Variables-DataTypes.kt
+++ 01-Main-Function-Variables-DataTypes.kt
@@ -1,4 +1,41 @@
 fun main(){
-    print("Hello World")
-    println("Amin Shahnai")
+//    println("Hello World")
+//    print("Amin Shahnai")
+
+    val test = 10
+    println(test)
+
+    val ashari = 10.5
+    println(ashari)
+
+    val matn = "AminShahnani"
+    println(matn)
+
+    val testBoolean = true
+//    val testBoolean = false
+    println(testBoolean)
+
+    val testBooleanFalse: Boolean = false
+    print(testBooleanFalse)
+
+    val testCharacter = 'A'
+    println(testCharacter)
+
+    val testString = "Amin Moammer Shahnani"
+    println(testString)
+
+    val numberSahih = 100
+    println(numberSahih)
+
+    val numberAshari = 12.5
+    println(numberAshari)
+
+    val testByte :Byte = 5
+    println(testByte)
+
+       
+
+
+
+
 }
