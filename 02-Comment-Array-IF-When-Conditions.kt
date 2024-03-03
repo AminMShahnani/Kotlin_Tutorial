4d7771598035172e58570b2fffca9fc045d4a21d
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-03 18:43:42 +0330

    Update

---
+++ 02-Comment-Array-IF-When-Conditions.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    // Comments
+
+    // One Line Comments
+
+    /*
+    * Multiple Line Comment
+    */
+
+    // Conditions
+
+    val grade = 15
+
+    if (grade > 15) {
+        println("Reject")
+    } else {
+        print("Accept")
+    }
+
+}
