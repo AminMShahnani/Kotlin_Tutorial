8c587cdefce60b6198417c7eea60af2e4227d3d2
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-03 18:46:40 +0330

    Update 02-Comment-Array-IF-When-Conditions.kt

--- 02-Comment-Array-IF-When-Conditions.kt
+++ 02-Comment-Array-IF-When-Conditions.kt
@@ -18,4 +18,15 @@ fun main(){
         print("Accept")
     }
 
+
+    /*
+    logical conditions
+        Less than: a < b
+        Less than or equal to: a <= b
+        Greater than: a > b
+        Greater than or equal to: a >= b
+        Equal to a == b
+        Not Equal to: a != b
+    */
+
 }
