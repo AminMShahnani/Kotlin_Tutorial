852f09c396818049d828350fe1ff5fab862eabe1
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-04 18:44:29 +0330

    Operators

--- 03-Operations-on-Variables.kt
+++ 03-Operations-on-Variables.kt
@@ -7,8 +7,19 @@ fun main() {
 
     val test :Char = 'A'
     val test_ascii :Int = test.toInt()
-    pritnln(test_ascii)
+    println(test_ascii)
+
+    val char_val = 97
+    println(a.toChar())
+
+    val num1 = 20
+    val num2 = 30
+    val result = num1 + num2
+    println(result)
+    println(num1 + num2)
+    println(num1 - num2)
+    println(num1 * num2)
+    println(num1 / num2)
 
     
-
 }
