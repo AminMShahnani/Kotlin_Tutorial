2df51676962a22c815b20dd3a1e27ed98a10a5f9
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-08 18:20:51 +0330

    Update 05-Array-Advanced_Loop.kt

--- 05-Array-Advanced_Loop.kt
+++ 05-Array-Advanced_Loop.kt
@@ -1,2 +1,12 @@
-class `05-Array-Advanced_Loop` {
+fun main(){
+
+    // Array
+    val scores :Array<Int> = arrayOf(17, 20, 16, 14, 5, 9, 10, 11, 14, 13)
+
+    ptintln( scores[0] )
+    
+
+
+
+
 }
