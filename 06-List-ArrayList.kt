740b6a88618e4468de40a4cd6e1412cd238b7545
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-08 18:40:37 +0330

    Update 06-List-ArrayList.kt

---
+++ 06-List-ArrayList.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+    //Array List -- Not fixed
+
+    var arrayList = arrayListOf(4, 2, 5, 8, 9, 6, 5, 3, 2, 1)
+
+    arrayList.forEach {
+        println(it)
+    }
+
+    arrayList.add(11)
+    arrayList.add(12)
+
+    arrayList.forEach {
+        println(it)
+    }
+
+    val testList = arrayList
+    arrayList.addAll()
+
+
+
+
+
+
+
+
+
+
+
+
+}
