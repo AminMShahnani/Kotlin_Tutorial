2bc2c911b80a9f8223e146ad1564049034890bfd
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-08 18:45:07 +0330

    Update 06-List-ArrayList.kt

--- 06-List-ArrayList.kt
+++ 06-List-ArrayList.kt
@@ -32,7 +32,16 @@ fun main(){
         println(it)
     }
 
+    //List
 
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    list.forEach {
+        println(it)
+    }
+
+    println(list)
+    
 
 
 
