9cd52de16a701cefcc45bb5178ec05a259535591
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-08 18:22:42 +0330

    Update 05-Array-Advanced_Loop.kt

--- 05-Array-Advanced_Loop.kt
+++ 05-Array-Advanced_Loop.kt
@@ -3,7 +3,16 @@ fun main(){
     // Array
     val scores :Array<Int> = arrayOf(17, 20, 16, 14, 5, 9, 10, 11, 14, 13)
 
-    ptintln( scores[0] )
+    println( scores[0] )
+
+    for (i in 0..9){
+        println(scores[i])
+
+        if(scores[i] > 15){
+            println(scores[i])
+        }
+    }
+
     
 
 
