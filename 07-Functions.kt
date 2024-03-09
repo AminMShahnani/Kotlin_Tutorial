8f990b5c61fbd87530d909c8ac12a918e315ead7
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-09 17:43:56 +0330

    Update 07-Functions.kt

--- 07-Functions.kt
+++ 07-Functions.kt
@@ -1,10 +1,28 @@
 fun printName(){
-
     println("Amin")
+}
 
+fun sumTwoNumber(a: Int, b: Int){
+    println(a + b)
 }
 
 fun main(){
 
     printName()
+
+    val num1 = readLine()!!.toInt()
+    val num2 = readLine()!!.toInt()
+    sumTwoNumber(num1, num2)
+
+
+
+
+
+
+
+
+
+
+
+
 }
