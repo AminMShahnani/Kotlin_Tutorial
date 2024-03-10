4ba77a86674af0c54377652727f7ae5108ba64bb
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-10 18:18:06 +0330

    Update 10-Class.kt

--- 10-Class.kt
+++ 10-Class.kt
@@ -1,23 +1,31 @@
-class Human() {
+class Human(name :String, family: String, mobile :String) {
 
-    lateinit var firstName :String
-    lateinit var familyName :String
-    lateinit var mobileNumber :String
+//    lateinit var firstName :String
+//    lateinit var familyName :String
+//    lateinit var mobileNumber :String
 
-    constructor(name :String, family: String, mobile :String) :this(){
+    var firstName :String = name
+    var familyName :String = family
+    var mobileNumber :String = mobile
 
-    }
+//    constructor(name :String, family: String, mobile :String) :this(){
+//        firstName = name
+//        familyName = family
+//        mobileNumber = mobile
+//    }
+//
+//    constructor(name :String, family :String): this() {
+//        firstName = name
+//        familyName = family
+//        mobileNumber = "No Mobile"
+//    }
 
-    constructor(name :String, family :String): this() {
-
-    }
-
-    init {
-        println("Hello calss human")
-
-        firstName = "${name} 100"
-
-    }
+//    init {
+//        println("Hello calss human")
+//
+//        firstName = "${name} 100"
+//
+//    }
 
     fun Intruduce(){
         println("My name is $firstName $familyName and my number is $mobileNumber ")
@@ -53,13 +61,35 @@ fun main() {
 
     val rudy = Human("Amin", "Shahnani", "0917")
 
-    rudy.Intruduce()
-    rudy.sayHello()
-    rudy.eatBreakfast()
-    rudy.goingToSchool()
-    rudy.eatFruit("Banana")
-    rudy.sayGoodBye()
-    rudy.sleep()
+//    rudy.Intruduce()
+//    rudy.sayHello()
+//    rudy.eatBreakfast()
+//    rudy.goingToSchool()
+//    rudy.eatFruit("Banana")
+//    rudy.sayGoodBye()
+//    rudy.sleep()
 
-    println(rudy.firstName)
+//    println(rudy.firstName)
+
+//    val amin = Human()
+//    amin.firstName = "Amin"
+//    amin.familyName = "Shah"
+//    amin.mobileNumber = "098"
+//
+//    val hassan = Human("Hassan", "Hassani", "09876")
+//
+//    val Samir = Human("Samir", "Samir")
+//    println(Samir.mobileNumber)
+
+    rudy.Intruduce()
+
+    // Visibility modifier : public - private - internal - protected
+    /*
+    *  Public : Show to all
+    *  Private : Show only to members of that class
+    *  Internal : Only show to this module
+    *  Protected : Only show to thos class member of child classes*/
+
+
+
 }
