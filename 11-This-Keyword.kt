2cdaed122b963bff9210986f9553a3eddf9cb5ad
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-10 18:31:35 +0330

    Update 11-This-Keyword.kt

---
+++ 11-This-Keyword.kt
@@ -0,0 +1,52 @@
+class People(val name :String, val family: String, val mobile :String) {
+
+
+//    lateinit var firstName :String
+//    lateinit var familyName :String
+//    lateinit var mobileNumber :String
+//
+//    init {
+//        this.firstName = name
+//        this.familyName = family
+//        this.mobileNumber = mobile
+//
+//    }
+
+    fun Intruduce(){
+        println("My name is $firstName $familyName and my number is $mobileNumber ")
+    }
+
+    fun sayHello(){
+        println("Hello Everybody")
+    }
+
+    fun sayGoodBye(){
+        println("Bye Bye")
+    }
+
+    fun eatBreakfast(){
+        println("I am eating breakfast")
+    }
+
+    fun goingToSchool(){
+        println("I am going to school")
+    }
+
+    fun sleep(){
+        println("I am sleeping")
+    }
+
+    fun eatFruit(nameFruit :String){
+        println("I am eating $nameFruit")
+    }
+}
+
+
+fun main() {
+
+    val rudy = People("Amin", "Shahnani", "0917")
+
+    rudy.Intruduce()
+
+
+}
