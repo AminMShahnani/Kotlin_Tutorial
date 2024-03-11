47d84f6ac8f286a0a00eb58d67b85fcd3b6c1bc9
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-11 21:17:25 +0330

    Update 15-Getter-Setter.kt

--- 15-Getter-Setter.kt
+++ 15-Getter-Setter.kt
@@ -1,3 +1,17 @@
+class BankAccount(firstPrice :String){
+     private var price :String = firstPrice
+
+//    fun getPrice() :String {
+//        return price + "USD"
+//    }
+//
+//    fun setPrice(newPrice :String) {
+//        price = newPrice
+//    }
+}
 fun main(){
-    
+
+    // getter - setter
+
+
 }
