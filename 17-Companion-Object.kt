18943e54ccfbc76a466bcbffa04fb01fd74e514c
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-11 21:32:02 +0330

    Update 17-Companion-Object.kt

--- 17-Companion-Object.kt
+++ 17-Companion-Object.kt
@@ -1,5 +1,24 @@
+class Students(n :String, f :String, i :String){
 
+    val name :String = n
+    val familyName :String = f
+    val idStudents :String = i
+
+    fun intriduce(){
+        println("My name is $name $familyName and my id is $idStudents")
+
+    }
+
+    companion object {
+        val nameOfUniversity = "MIT"
+    }
+}
 
 fun main(){
-    
+    val amin = Students("Amin", "Shah", "123")
+    amin.intriduce()
+
+    val university = Students.nameOfUniversity
+
+    println(university)
 }
