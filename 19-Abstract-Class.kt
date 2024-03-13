4dadd3372ea4ff780685407d2e2cb145c9885691
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-13 19:23:40 +0330

    Update 19-Abstract-Class.kt

--- 19-Abstract-Class.kt
+++ 19-Abstract-Class.kt
@@ -1,2 +1,21 @@
+abstract class Animal (){
 
+    abstract fun talk()
 
+}
+
+class Goat() :Animal() {
+    override fun talk() {
+        println("MA MA")
+    }
+}
+
+class Giraffe() :Animal() {
+    override fun talk() {
+        println("HUM HUM")
+    }
+}
+
+fun main() {
+
+}
