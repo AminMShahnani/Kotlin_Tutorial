4a0df37e12e3943bf2e0274c90ed9a7b08e109ce
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-13 19:31:01 +0330

    Update StudentsClass.kt

--- StudentsClass.kt
+++ StudentsClass.kt
@@ -1,2 +1,10 @@
-class StudentsClass {
+class StudentsClass (first_name :String, teacher :String) {
+
+    lateinit var name :String
+    lateinit var nameOfTeacher :String
+
+    init {
+        name = first_name
+        nameOfTeacher = teacher
+    }
 }
