a66ad41bc835b75ed50d3bf1a372ac963f324638
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-13 19:36:52 +0330

    Update 20-Data-Class.kt

--- 20-Data-Class.kt
+++ 20-Data-Class.kt
@@ -4,4 +4,11 @@ fun main() {
     // Data Class
 
     val amin = Student("Amin", "Shah")
+    val amin = Student("Amin", "Shah")
+
+    if(amin == amin){
+        println("YES")
+    } else {
+        println("NO")
+    }
 }
