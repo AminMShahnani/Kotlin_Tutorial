fd64bf8d73d000deaa1473db0e13ab3b6349e3db
aminmshahnani <mshahnani.amin@gmail.com>
2024-03-26 19:57:15 +0330

    Update 21-Interface.kt

---
+++ 21-Interface.kt
@@ -0,0 +1,13 @@
+ class Class_A(){
+
+     fun getData(){
+         val data :List<String> = listOf(
+             "Amin",
+             "Saeed",
+             "Sara",
+             "Samin",
+             "Aida",
+             "Ali"
+         )
+     }
+ }
