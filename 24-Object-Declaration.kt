ca28ba23d4979509757550b78b38c3b705d2e218
Amin Shahnani <112487613+AminMShahnani@users.noreply.github.com>
2024-04-21 20:27:50 +0330

    Create 24-Object-Declaration.kt

---
+++ 24-Object-Declaration.kt
@@ -0,0 +1,19 @@
+interface BaseInterface {
+
+    fun printHello()
+
+    fun printBye()
+}
+
+fun main() {
+    val test = object :BaseInterface {
+
+        override fun printHello() {
+            println("Print Hello")
+        }
+
+        override  fun printBye() {
+            println("Print Bye")
+        }
+    }
+}
