e3c1ceac28ec6113ce8b5f590c7db103ad3004b3
Amin Shahnani <112487613+AminMShahnani@users.noreply.github.com>
2024-04-21 22:11:06 +0330

    Update 30-Delegated-Properties.kt

--- 30-Delegated-Properties.kt
+++ 30-Delegated-Properties.kt
@@ -1,12 +1,22 @@
+import kotlin.properties.Delegates
+
 class Test {
 
     val name :String by lazy {
         println("Lazy")
         "Amin"
     }
+
+    var age :Int by Delegates.observable(20) {
+        property, oldValue, newValue ->
+
+        println("Old value : $oldValue \n new value : $newValue")
+    }
 }
 
 fun main() {
     val test = Test()
     println(test.name)
+
+    test.age = 100
 }
