de2e4252bec9a1b843c6ee8eb40e2d0f15c9974e
Amin Shahnani <112487613+AminMShahnani@users.noreply.github.com>
2024-04-21 19:52:30 +0330

    Update 23-Generics.kt

--- 23-Generics.kt
+++ 23-Generics.kt
@@ -6,10 +6,16 @@ fun <T> printingMany(x :T, y :T, z :T) {
     println(" $x $y $z")
 }
 
+fun <T> printingM(x :T, y :T, z :T, age: Int) {
+    println(" $x $y $z $age")
+}
+
 fun main(){
     printing(12)
     printing("Amin")
     printing(12.34)
 
     printingMany(2.13, 2, "Amin")
+
+    printingM(2.13, 2, "Amin", 38)
 }
