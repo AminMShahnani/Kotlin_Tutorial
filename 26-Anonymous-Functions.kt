50c63556f80ff1e2ddcbc26c252f904516d81606
Amin Shahnani <112487613+AminMShahnani@users.noreply.github.com>
2024-04-21 21:47:59 +0330

    Update 26-Anonymous-Functions.kt

--- 26-Anonymous-Functions.kt
+++ 26-Anonymous-Functions.kt
@@ -7,6 +7,10 @@ fun main() {
         acc, i -> acc + 1
     }
 
+    val result_lambda_return = list.fold(1000) {
+        acc, i -> return@fold acc + 1
+    }
+
     // anonymous
     val result_anon = list.fold(1000, fun(acc: Int, i: Int): Int {
         return acc + i
